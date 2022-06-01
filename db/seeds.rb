04b21c58ee959d169fc519c449383282dac51f01
Paola Moreno Reyes <nailamoreno99@gmail.com>
2022-06-01 14:44:02 -0500

    currentOrder seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 puts "creating muggle shopping wizards"
 
-User.create(firstname: "Hermione", lastname: "Granger", email: "user1@gmail.com", password: "password")
-User.create(firstname: "Harry", lastname: "Potter", email: "user2@gmail.com", password: "password")
-User.create(firstname: "Ron", lastname: "Weasley", email: "user3@gmail.com", password: "password")
-User.create(firstname: "Serious", lastname: "Black", email: "user4@gmail.com", password: "password")
-User.create(firstname: "Lily", lastname: "Potter", email: "user5@gmail.com", password: "password")
+User.create(firstname: "Hermione", lastname: "Granger", email: "user1@gmail.com", password: "password", current_order: "null")
+User.create(firstname: "Harry", lastname: "Potter", email: "user2@gmail.com", password: "password", current_order: "null")
+User.create(firstname: "Ron", lastname: "Weasley", email: "user3@gmail.com", password: "password", current_order: "null")
+User.create(firstname: "Serious", lastname: "Black", email: "user4@gmail.com", password: "password", current_order: "null")
+User.create(firstname: "Lily", lastname: "Potter", email: "user5@gmail.com", password: "password", current_order: "null")
 
 puts "done ☺"
 
